48d61767e90f551fe7d498642408ccddfd972366
Matthew Grant <matt@mattgrant.net.nz>
2019-01-06 22:42:25 +1300

    WIP - magcode-core boilerplate daemon

---
+++ scripts/globals_.py
@@ -0,0 +1,26 @@
+"""
+Globals file for zsnapd
+"""
+
+from magcode.core.globals_ import settings
+
+# settings for where files are
+settings['config_dir'] = '/etc/zsnapd'
+settings['log_dir'] = '/var/log'
+settings['run_dir'] = '/var/run'
+settings['config_file'] = settings['config_dir'] + '/' + 'zsnapd.conf'
+# Zsnapd only uses one daemon
+settings['pid_file'] = settings['run_dir'] + '/' + 'zsnapd.pid'
+settings['log_file'] = settings['log_dir'] \
+        + '/' + settings['process_name'] + '.log'
+settings['panic_log'] = settings['log_dir'] \
+        + '/' + settings['process_name'] + '-panic.log'
+
+# zsnapd.py
+# Print debug mark
+settings['debug_mark'] = False
+# Number of seconds we wait while looping in main loop...
+settings['sleep_time'] = 3 # seconds
+settings['debug_sleep_time'] = 20 # seconds
+
+
---
+++ scripts/zsnapd.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+# Copyright (c) 2018 Matthew Grant <matt@mattgrant.net.nz>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import os
+import os.path
+import errno
+import sys
+import pwd
+import time
+import copy
+import signal
+import gc
+import json
+
+import psutil
+
+# A bit of nice stuff to set up ps output as much as we can...
+try:
+    from setproctitle import getproctitle
+    setproctitle_support = True
+except ImportError:
+    setproctitle_support = False
+
+from magcode.core.process import ProcessDaemon
+from magcode.core.process import SignalHandler
+from magcode.core.globals_ import *
+from magcode.core.utility import get_numeric_setting
+from magcode.core.utility import get_boolean_setting
+# import this to set up config file settings etc
+import scripts.globals_
+
+USAGE_MESSAGE = "Usage: %s [-dhv] [-c config_file]"
+COMMAND_DESCRIPTION = "ZFS Snap Managment Daemon"
+
+class ZsnapdProcess(ProcessDaemon):
+    """
+    Process Main Daemon class
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(usage_message=USAGE_MESSAGE,
+            command_description=COMMAND_DESCRIPTION, *args, **kwargs)
+
+    def main_process(self):
+        """
+        Main process for zfssnapd
+        """
+
+        if (settings['rpdb2_wait']):
+            # a wait to attach with rpdb2...
+            log_info('Waiting for rpdb2 to attach.')
+            time.sleep(float(settings['rpdb2_wait']))
+
+        log_info('program starting.')
+        log_debug("The daemon_canary is: '%s'" % settings['daemon_canary'])
+        # Do a nice output message to the log
+        pwnam = pwd.getpwnam(settings['run_as_user'])
+        if setproctitle_support:
+            gpt_output = getproctitle()
+        else:
+            gpt_output = "no getproctitle()"
+        log_debug("PID: %s process name: '%s' daemon: '%s' User: '%s' UID: %d GID %d" 
+                % (os.getpid(), gpt_output, self.i_am_daemon(), pwnam.pw_name,
+                    os.getuid(), os.getgid()))
+
+        if (settings['memory_debug']):
+            # Turn on memory debugging
+            log_info('Turning on GC memory debugging.')
+            gc.set_debug(gc.DEBUG_LEAK)
+
+       # Create a Process object so that we can check in on ourself resource
+        # wise
+        self.proc_monitor = psutil.Process(pid=os.getpid())
+
+        # Initialise  a few nice things for the loop
+        debug_mark = get_boolean_setting('debug_mark') 
+        sleep_time = get_numeric_setting('sleep_time', float)
+
+        # Process Main Loop
+        while (self.check_signals()):
+            
+            if debug_mark:
+                log_debug("----MARK---- sleep(%s) seconds ----"
+                        % sleep_time) 
+            time.sleep(sleep_time)
+
+        log_info('Exited main loop - process terminating normally.')
+        sys.exit(os.EX_OK)
+
+if (__name__ is "__main__"):
+    exit_code = ZsnapdProcess(sys.argv, len(sys.argv))
+    sys.exit(exit_code)
+
+
+
